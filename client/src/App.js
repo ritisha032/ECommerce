51307bb35f1a05948d3394d74422bc01537b3881
ritisha032 <ritishasingh74@gmail.com>
2023-07-03 12:09:13 +0530

    .

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,19 @@
-import './App.css';
-import Layout from "./components/layouts/Layout";
-
+import {Routes,Route} from "react-router-dom"
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Contact from "./pages/Contact";
+import Policy from "./pages/Policy";
+import PageNotFound from "./pages/PageNotFound";
 function App() {
   return (
     <>
-     
-      <Layout>
-        <h1>Ecommerce App</h1>
-      </Layout>
+      <Routes>
+        <Route path="/" element={<Home/>}></Route>
+        <Route path="/about" element={<About/>}></Route>
+        <Route path="/contact" element={<Contact/>}></Route>
+        <Route path="/policy" element={<Policy/>}></Route>
+        <Route path="*" element={<PageNotFound/>}></Route>
+      </Routes>
     </>
   );
 }
